58e309d8fd5bdd561738ebf7c6c8b9831bdb5b26
汪培良 <peiliang.wang@marcpoint.com>
2020-06-28 10:09:14 +0800

    生成链表

---
+++ linked_ist.js
@@ -0,0 +1,62 @@
+// linked list
+
+// 单链表
+function oneLinkedList(){
+    const linkedList = [1, 2, 3, 4, 5, 6];
+
+    class Node {
+      constructor(val, next) {
+        this.val = val;
+        this.next = next;
+      }
+    }
+
+    let node = new Node(linkedList[0]);
+    let prevNode = node;
+    let thatNode = null;
+    for (let i = 1; i<linkedList.length;i++){
+        thatNode = new Node(linkedList[i], null);
+        prevNode.next = thatNode;
+        prevNode = thatNode;
+    }
+    console.log(node);
+
+    let ptNode = node;
+    while (true) {
+      console.log(ptNode.val);
+      if (ptNode.next) {
+        ptNode = ptNode.next;
+      } else {
+        break;
+      }
+    }
+}
+
+// oneLinkedList()
+
+// 双链表 循环链表
+function bothLinkedList(){
+    const linkedList = [1, 2, 3, 4, 5, 6];
+
+    class Node {
+        constructor(val,prev, next) {
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+        }
+    }
+    let node = new Node(linkedList[0], null, null);
+    let prevNode = node;
+    let thatNode = null;
+    for (let i = 1; i < linkedList.length; i++) {
+        thatNode = new Node(linkedList[i],prevNode, null);
+        prevNode.next = thatNode;
+        prevNode = thatNode;
+    }
+    node.prev = thatNode;
+    prevNode.next = node
+  
+}
+
+bothLinkedList()
+
