9c35ee233ee7de00a26909f7ac8a11acc0e1a335
汪培良 <peiliang.wang@marcpoint.com>
2020-07-07 17:31:13 +0800

    更新1、155解法

--- leetCode.js
+++ leetCode.js
@@ -43,39 +43,22 @@ function leetCode215(){
 
 function leetCode1(){
     var twoSum = function (nums, target) {
-      let numsLen = nums.length - 1;
-      let num = 0;
-      let sunArr = [];
-      let newArr = nums.filter((_) => target > 0 ? _ > target : _ < target );
-      while (num <= numsLen) {
-          console.log(nums[num], target);
-        // if (
-        //   (target >= 0 && nums[num] > target) ||
-        //   (target < 0 && nums[num] < target)
-        // ) {
-        //   num++;
-        //   continue;
-        // }
-        const cha = target - nums[num];
-        console.log(cha, target, nums[num]);
-        let mounIndex = -1;
-        for (let i = num + 1; i <= numsLen; i++) {
-            console.log(nums[i] === cha, nums[i]);
-          if (nums[i] === cha) {
-            mounIndex = i;
-            continue;
-          }
-        }
-        if (mounIndex >= 0) {
-          sunArr = [num, mounIndex];
-          break;
-        } else {
-          num++;
+      let hashTable = {};
+      for(const [key, value] of nums.entries()){
+        hashTable[value] = {
+          index: key,
+          val: value,
+        };
+      }
+      for(let i = 0; i < nums.length;i++){
+        const difference = target - nums[i];
+        if (!!hashTable[difference] && hashTable[difference]['index'] !== i) {
+          return [i, hashTable[difference]["index"]];
         }
       }
-      return sunArr;
+      return [];
     };
-    console.log(twoSum([-18, 12, 3, 0], -6));
+    console.log(twoSum([3, 2, 4], 6));
 }
 
 // leetCode1()
@@ -245,4 +228,140 @@ function leetCode146_v2() {
   //    obj.put(key,value)
 }
 
-leetCode146_v2()
+// leetCode146_v2()
+function leetCode1470(nums, n) {
+  let newList = []
+  for(let i = 0; i < n; i++){
+    newList.push(nums[i]);
+    newList.push(nums[i + n]);
+  }
+  return newList;
+};
+
+// console.log(leetCode1470([2, 5, 1, 3, 4, 7], 3));
+
+function leetCode14731(candies, extraCandies) {
+  const Max = Math.max(...candies);
+  return candies.map((_) => _ + extraCandies >= Max);
+}
+
+// leetCode14731([2, 3, 5, 1, 3], 3);
+
+// function leetCode1486(n, start) {}
+
+function leetCode378(matrix, k) {
+  // const matrixLen = matrix[0].length;
+  // const lenNum = k === matrixLen ? 0 : parseInt(k / matrixLen)
+  // if (lenNum  === 0){
+  //   return matrix[0][k-1]
+  // }else{
+  //   return matrix[lenNum][k % (matrixLen * lenNum) - 1];
+  // }
+  return matrix
+    .join()
+    .split(",")
+    .sort((a, b) => Number(a) - Number(b))[k - 1];
+
+
+
+  //   if (k > matrixLen) {
+  //     fetch = lenNum > 0 ? k % (matrixLen * lenNum) : k % matrixLen;
+  //   } else {
+  //     fetch = k;
+  //   }
+  // return matrix[lenNum > 0 ? lenNum - 1 : 0][fetch];
+};
+
+// console.log(
+//   leetCode378(
+//     [
+//       [1, 5, 9],
+//       [10, 11, 13],
+//       [12, 13, 15],
+//     ],
+//     8
+//   )
+// );
+// console.log(leetCode378([[-5]], 1));
+// console.log(
+//   leetCode378(
+//     [
+//         [1, 2],
+//         [1, 3],
+//     ],
+//     2
+//   )
+// );
+
+
+
+
+// var mergeTwoLists = function (l1, l2) {
+//   let newLinked = ListNode();
+//   let linkedFoot = null;
+//   let nodeOne = l1;
+//   while (nodeOne) {
+//     nodeOne = nodeOne.next;
+//   }
+// };
+
+function leetcode155(){
+  var MinStack = function () {
+    this.stack = [];
+    this.length = 0;
+    this.minList = [];
+  };
+
+  MinStack.prototype.push = function (x) {
+    const { minList } = this
+    const len = minList.length;
+    minList.push(x);
+    if( len > 0 && minList[len - 1] > x){
+      for (let i = len - 1; i >= 0; i--) {
+        if (minList[i] > x) {
+          const item = minList[i];
+          minList[i] = x;
+          minList[i + 1] = item;
+        } else {
+          continue
+        }
+      }
+    }
+    this.stack.push(x);
+    this.length++;
+  };
+   
+  MinStack.prototype.pop = function () {
+    const {length,minList,stack} = this
+    const len = length - 1
+    const top = stack[len]
+    const minIndex = minList.indexOf(top);
+    if (minIndex>=0){
+      this.minList.splice(minIndex,1);
+    } 
+   this.stack.splice(len, 1);
+    this.length--;
+  };
+
+  MinStack.prototype.top = function () {
+    return this.stack[this.length - 1];
+  };
+
+  MinStack.prototype.getMin = function () {
+    return  this.minList.length > 0 ? this.minList[0] : null
+  };
+    const minStack = new MinStack();
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    minStack.pop();
+    console.log(minStack.stack);
+    console.log(minStack.top());
+    // minStack.getMin();  
+    // minStack.pop();
+    // minStack.top();     
+    // minStack.getMin();   
+
+}
+
+// leetcode155()
